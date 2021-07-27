cd1804e4c5fc34e751f9ff2bad531cc7fb12454c
seoeunkong <seoeun7872@gmail.com>
2021-07-27 17:33:23 +0900

    총

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int damage;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,6 +11,12 @@ public class Player : MonoBehaviour
     bool wDown;
     bool jDown;
     bool isJump;
+    bool sDown1; //숫자 1를 눌렀을때
+    bool sDown2; //숫자 2를 눌렀을때
+
+    //플레이어 무기 관련 배열
+    public GameObject[] weapons;
+    public bool[] hasWeapons;
 
     public int health;
     public int maxHealth;
@@ -24,7 +30,10 @@ public class Player : MonoBehaviour
     Rigidbody rigid;
     Animator anim;
 
-    GameObject nearObject; //트리거된 무기들을 저장하기 위한 변수
+    GameObject nearWeapon; //트리거된 무기들을 저장하기 위한 변수
+    Weapon equipWeapon; //기존에 장착된 무기를 저장하는 변수
+    int equipWeaponIndex=-1;
+   
 
     void Awake()
     {
@@ -40,7 +49,9 @@ public class Player : MonoBehaviour
         //캐릭터의 수직속도(중력)을 적용한다
         yVelocity += gravity * Time.deltaTime;
         moveVec.y = yVelocity;
-
+        Interaction_W();//무기 상호작용
+        Swap();
+        Attack();
     }
     void GetInput()
     {
@@ -48,6 +59,8 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
+        sDown1 = Input.GetButtonDown("sDown1");
+        sDown2 = Input.GetButtonDown("sDown2");
     }
 
     //플레이어 움직이게 하는 함수
@@ -78,6 +91,62 @@ public class Player : MonoBehaviour
             anim.SetBool("isJump",false);
             anim.SetTrigger("doJump");
             isJump = true;
+        }
+    }
+    void Swap() //무기 교체
+    {
+        // 무기를 갖고 있지 않는 상태에서 무기가 활성화되는 것을 방지
+        if (sDown1 && (!hasWeapons[0]||equipWeaponIndex==0))
+        {
+            return;
+        }
+        if (sDown2 && (!hasWeapons[1]||equipWeaponIndex==1))
+        {
+            return;
+        }
+
+        int weaponIndex = -1;
+        //숫자 1을 누르면 총, 숫자2를 누르면 수류탄
+        if (sDown1) weaponIndex = 0;
+        if (sDown2) weaponIndex = 1;
+
+        //1,2를 누르면 무기가 보이게 함.
+        if (sDown1 || sDown2)
+        {
+            if (equipWeapon != null) //빈손이 아니라면 
+            {
+                equipWeapon.gameObject.SetActive(false); //전에 손에 쥐고 있었던 무기를 비활성화함
+            }
+
+            equipWeaponIndex = weaponIndex;
+            equipWeapon = weapons[weaponIndex].GetComponent<Weapon>();
+            equipWeapon.gameObject.SetActive(true); // 손에 쥐고 있는 무기를 활성화하여 보이게함.
+        }
+    }
+
+    void Attack() //공격
+    {
+        if (equipWeapon == null) //무기를 갖고 있는 경우에만 실행
+            return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            equipWeapon.Use();
+            anim.SetTrigger("doShot");
+        }
+    }
+
+    void Interaction_W() //닿은 무기들을 배열에 저장
+    {
+        if (nearWeapon != null)
+        {
+            if (nearWeapon.tag == "Weapon")
+            {
+                Item item = nearWeapon.GetComponent<Item>();
+                int weaponIndex = item.value;
+                hasWeapons[weaponIndex] = true; //해당 무기가 플레이어한테 갖고 있다고 설정
+
+                Destroy(nearWeapon);
+            }
         }
     }
 
@@ -115,6 +184,22 @@ public class Player : MonoBehaviour
             }
             //아이템은 닿으면 사라짐
             Destroy(other.gameObject);
+        }
+    }
+
+    void OnTriggerStay(Collider other) //무기 콜라이더에 닿으면(=무기 오브젝트 감지)
+    {
+        if (other.tag == "Weapon")
+        {
+            nearWeapon = other.gameObject;
+        }
+    }
+
+    void OnTriggerExit(Collider other) //무기 콜라이더에서 빠져나오면
+    {
+        if (other.tag == "Weapon")
+        {
+            nearWeapon = null;
         }
     }
 }
