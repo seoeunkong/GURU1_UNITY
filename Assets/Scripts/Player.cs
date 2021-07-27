86e07f445afb2adaed227e67bea9fbeeae97d9c2
seoeunkong <seoeun7872@gmail.com>
2021-07-27 12:29:44 +0900

    동주님+나영님(아이템)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,19 +6,14 @@ public class Player : MonoBehaviour
 {
     public float speed;
     public float JumpPower;
-
-    public GameObject[] weapons;
-    public bool[] hasWeapons;
-
     float hAxis;
     float vAxis;
     bool wDown;
     bool jDown;
-    bool isDown1;
-    bool isDown2;
     bool isJump;
-    bool isFireReady;
-    bool isChange;
+
+    public int health;
+    public int maxHealth;
 
     //중력 변수
     public float gravity = -20.0f;
@@ -28,14 +23,6 @@ public class Player : MonoBehaviour
 
     Rigidbody rigid;
     Animator anim;
-
-    //트리거된 무기를 저장하기 위한 변수
-    GameObject nearweapon;
-    Weapons equipWeapon; //이미 갖고있는 무기
-    int equipWeaponIndex;
-
-    float fireDelay;
-
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
@@ -50,21 +37,17 @@ public class Player : MonoBehaviour
         //캐릭터의 수직속도(중력)을 적용한다
         yVelocity += gravity * Time.deltaTime;
         moveVec.y = yVelocity;
-        w_Interation();
-        Change(); //무기 교체
-        Attack(); //공격
-    }
 
+    }
     void GetInput()
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
         jDown = Input.GetButtonDown("Jump");
-        isDown1 = Input.GetButtonDown("isDown1"); //숫자 1를 눌렀을 때
-        isDown2 = Input.GetButtonDown("isDown2"); //숫자 2를 눌렀을 때
-
     }
+
+    //플레이어 움직이게 하는 함수
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
@@ -82,7 +65,8 @@ public class Player : MonoBehaviour
         anim.SetBool("isRun", moveVec != Vector3.zero);
         anim.SetBool("isWalk", wDown);
     }
-
+    
+    //플레이어 점프하게 하는 함수
     void Jump()
     {
         if(jDown&&!isJump)
@@ -94,69 +78,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    void w_Interation()
-    {
-        if (nearweapon != null)
-        {
-            if (nearweapon.tag == "Weapon") //무기와 닿으면
-            {
-                NotUseWeapons w = nearweapon.GetComponent<NotUseWeapons>();
-                int weaponIndex = w.value;
-                hasWeapons[weaponIndex] = true; //헤당 번호의 무기를 갖고 있는 것으로 설정
-
-                Destroy(nearweapon);
-            }
-        }
-    }
-
-    void Change()
-    {
-        int weaponIndex = -1;
-        if (isDown1) weaponIndex = 0;
-        if (isDown2) weaponIndex = 1;
-
-        //갖고 있는 무기가 없는 상황에서 무기가 교체되는 것을 방지
-        if (isDown1 && (!hasWeapons[0] ))
-        {
-            Debug.Log(equipWeaponIndex);
-            return;
-        }
-        if (isDown2 && (!hasWeapons[1] ))
-            return;
-
-        if ((isDown1 || isDown2) && !isJump) //숫자1 또는 2를 누르는 경우에(점프하는 경우엔 제외) 무기 활성화
-        {
-            if (equipWeapon != null) //손에 무기가 겹치게 드는 것을 방지
-            {
-                equipWeapon.gameObject.SetActive(false);
-            }
-            equipWeaponIndex = weaponIndex;
-            equipWeapon = weapons[weaponIndex].GetComponent<Weapons>();
-            equipWeapon.gameObject.SetActive(true);
-
-           
-        }
-    }
-
-    void Attack()
-    {
-        if (equipWeapon == null)
-            return;
-
-        //공격가능 여부확인
-        //fireDelay += Time.deltaTime;
-        //isFireReady = equipWeapon.rate < fireDelay;
-
-        if (Input.GetMouseButtonDown(0)) //왼쪽 마우스 누르면 총 발사
-        {
-            equipWeapon.Use();
-            anim.SetTrigger("doShot");
-            //fireDelay = 0;
-        }
-
-       
-    }
-
+    //플레이어가 Floor로 태그된 물체와 닿으면 착지하는 모션을 멈춤
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Floor")
@@ -167,22 +89,29 @@ public class Player : MonoBehaviour
 
     }
 
-    void OnTriggerStay(Collider w)
+    //플레이어가 아이템이라고 태그된 물체와 닿으면 일어나는 일
+    private void OnTriggerEnter(Collider other)
     {
-        // 만약 충돌한 것이 무기이면
-        if (w.tag == "Weapon")
+        if (other.tag == "Item")
         {
-            nearweapon = w.gameObject;
+            Item item = other.GetComponent<Item>();
+            switch (item.type)
+            {
+                //아이템 스크립트에서 지정한 타입과 value 값에 따라 health 가 늘어남
+                //health 가 maxhealth를 넘어서면 health 값이 maxhealth 값이 됨
+                case Item.Type.Ammo:
+                    health += item.value;
+                    if (health > maxHealth)
+                        health = maxHealth;
+                    break;
+                case Item.Type.Heart:
+                    health += item.value;
+                    if (health > maxHealth)
+                        health = maxHealth;
+                    break;
+            }
+            //아이템은 닿으면 사라짐
+            Destroy(other.gameObject);
         }
     }
-
-    void OnTriggerExit(Collider w)
-    {
-        if (w.tag == "Grenade" || w.tag == "Gun")
-        {
-            nearweapon = null;
-        }
-
-    }
-
 }
